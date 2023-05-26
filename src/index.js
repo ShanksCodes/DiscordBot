2aaa0737ebc55b70c299633919a6c683a849c86c
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-05-26 20:48:04 +0530

    z

--- src/index.js
+++ src/index.js
@@ -14,4 +14,23 @@ client.on('ready', (c) => {
 console.log(`✅ ${c.user.tag} is online.`);
 });  
 
+client.on('interactionCreate', (interaction)=>{
+    if(!interaction.isCommand())
+        return;
+    
+    if(interaction.commandName==='hey'){
+        interaction.reply('hey!');
+    } 
+    
+    if(interaction.commandName==='ping'){
+        interaction.reply('Pong!');
+    }
+
+    if(interaction.commandName==='add'){
+        const firstNumber = interaction.options.get('first-number').value;
+        const secondNumber = interaction.options.get('second-number').value;
+        interaction.reply(` Sum of ${firstNumber} and ${secondNumber} is ${firstNumber+secondNumber}`);
+    }
+})
+
 client.login(process.env.TOKEN);
---
+++ src/register-commands.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const {REST, Routes, ApplicationCommandOptionType} = require('discord.js');
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with hey!',
+    },    
+    
+    {
+        name: 'ping',
+        description: 'replies with Pong!',
+    },
+
+    {
+        name: 'add',
+        description: 'adds two numbers',
+        options: [
+            {
+                name: 'first-number',
+                description: 'first number',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+                choices: [
+                    { name: 'One', value: '1' },
+                    { name: 'Two', value: '2' },
+                ]
+            },
+
+            {
+                name: 'second-number',
+                description: 'second number',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+        ],
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
+
+(async () => {
+    try{
+      console.log('Registering slash commands...');
+
+      await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+        {body: commands},
+      )
+      console.log('slash commands were registered successfully.');
+    }
+    catch(error){
+        console.error(`There was an error: ${error}`);
+    }
+})();;
